26f5f39f6f53dd8a185e8d9e3116d5991706e848
Marina Ildutova <pagum_marina@mail.ru>
2024-10-02 23:57:24 +1000

    fix main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.widget import get_date, mask_account_card
 
 print(mask_account_card("Maestro 1596837868705199"))
-print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("Счет 35383033474447895560"))
 print(get_date("2024-03-11T02:26:18.671407"))
