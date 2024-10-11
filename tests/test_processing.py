875b327eb067d62ff7d6143f9251c5fa29b6e055
Marina Ildutova <pagum_marina@mail.ru>
2024-10-11 17:42:15 +1000

    обрабтка функций линтерами

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,26 +1,25 @@
-
-import pytest
-from src.processing import  filter_by_state, sort_by_date
+from src.processing import filter_by_state, sort_by_date
 
 
-def test_filter_by_state(list_dict):
+def test_filter_by_state(list_dict: list) -> None:
     assert filter_by_state(list_dict) == [
-             {"id": "41428829", "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-             {"id": "939719570", "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
- ]
+        {"id": "41428829", "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": "939719570", "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
 
-def test_filter_by_state_none():
+
+def test_filter_by_state_none() -> None:
     assert filter_by_state([]) == []
 
 
-
-def test_sort_by_date(list_dict_data):
-    assert sort_by_date(list_dict_data) ==  [
+def test_sort_by_date(list_dict_data: list) -> None:
+    assert sort_by_date(list_dict_data) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
-def test_sort_by_date_none():
+
+def test_sort_by_date_none() -> None:
     sort_by_date([]) == []
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,7 +1,11 @@
 import pytest
-from src.widget import mask_account_card, get_date
 
-@pytest.mark.parametrize( "number,result", [
+from src.widget import get_date, mask_account_card
+
+
+@pytest.mark.parametrize(
+    "number,result",
+    [
         ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
         ("Счет 64686473678894779589", "Счет **9589"),
         ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
@@ -10,15 +14,19 @@ from src.widget import mask_account_card, get_date
         ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
         ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
         ("Счет 73654108430135874305", "Счет **4305"),
-])
-
-def test_mask_account_card(number,result):
+    ],
+)
+def test_mask_account_card(number: str, result: str) -> None:
     assert mask_account_card(number) == result
 
-@pytest.mark.parametrize( "old_data, data_slice_join", [
-    ("2018-07-11T02:26:18.671407", "11.07.2018"),
-    ("2019-07-03T18:35:29.512364", "03.07.2019"),
-    ("2018-06-30T02:08:58.4255722", "30.06.2018"),
-    ])
-def test_get_date(old_data, data_slice_join):
+
+@pytest.mark.parametrize(
+    "old_data, data_slice_join",
+    [
+        ("2018-07-11T02:26:18.671407", "11.07.2018"),
+        ("2019-07-03T18:35:29.512364", "03.07.2019"),
+        ("2018-06-30T02:08:58.4255722", "30.06.2018"),
+    ],
+)
+def test_get_date(old_data: str, data_slice_join: str) -> None:
     assert get_date(old_data) == data_slice_join
